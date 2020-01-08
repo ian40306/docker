05778fe6ca9744a939648a9235b8a347387f680d
ian40306 <43855922+ian40306@users.noreply.github.com>
2020-01-08 22:57:39 +0800

    Update ssh_setup.sh

--- ubuntu1804/ssh_setup.sh
+++ ubuntu1804/ssh_setup.sh
@@ -1,6 +1,7 @@
 apt-get update
 apt-get install -y openssh-server
 echo "PermitRootLogin yes">>/etc/ssh/sshd_config
+echo "/etc/init.d/ssh start">>~/.bashrc
 /etc/init.d/ssh restart
 echo ""
 echo "Please enter the ssh password"
